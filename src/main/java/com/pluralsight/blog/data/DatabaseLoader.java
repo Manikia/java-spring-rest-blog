dd7374306885c1f97422a478e6eddea45ec6ac27
Monika <monikaangel.ma15@gmail.com>
2022-04-19 16:56:24 -0700

    Completed module

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -15,12 +15,7 @@ import java.util.stream.IntStream;
 @Component
 public class DatabaseLoader implements ApplicationRunner {
 
-    @Autowired
     private final PostRepository postRepository = this.postRepository;
-
-
-
-
 
     private final String[] templates = {
             "Smart Home %s", "Mobile %s - For When You're On he Go", "The %s - Your New Favorite Accessory"};
@@ -29,7 +24,10 @@ public class DatabaseLoader implements ApplicationRunner {
     public List<Post> randomPosts = new ArrayList<>();
     public List<Author> authors = new ArrayList<>();
 
-    public DatabaseLoader() {
+    @Autowired
+    public DatabaseLoader(PostRepository postRepository) {
+        PostRepository PostRepository = this.postRepository;
+        postRepository.saveAll(randomPosts);
     }
 
     @Override
@@ -41,8 +39,6 @@ public class DatabaseLoader implements ApplicationRunner {
             String title = String.format(template, gadget);
             Post post = new Post(title, "Lorem ipsum dolor sit amet, consectetur adipiscing elit… ");
             randomPosts.add(post);
-
-            postRepository.saveAll(randomPosts);
         });
     }
 }
